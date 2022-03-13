9e1cf4df63709f59f5e8843d6d19a1889af8fbfb
veraz00 <83570482+veraz00@users.noreply.github.com>
2022-03-13 15:04:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config(object):
     BOOTSTRAP_SERVE_LOCAL = True
     SQLACHEMY_TRACK_MODICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    IMAGE_FOLDER = os.path.join(basedir, "input")  # current: /Users/zenglinlin/Dev/linlin-server/run.py
 class ProductionConfig(Config):
     DEBUG = False
     
