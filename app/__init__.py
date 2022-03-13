cf7d297cb1bb1bae69d17cbeb9f6e8cfa1d8c18c
veraz00 <83570482+veraz00@users.noreply.github.com>
2022-03-13 15:04:51 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,30 +7,6 @@ import os
 import base64
 import click
 from flask import session, g
-
-
-
-def fake_images():
-    record = dict()
-    for file in os.listdir(os.environ['SAMPLE_DIR']):
-        record['filename'] = file
-        with open(os.path.join(os.environ['SAMPLE_DIR'], file), 'rb') as image1:
-            record['content'] = base64.b64encode(image1.read())
-
-            from controller.image import ImageController
-            g.image = ImageController()
-            g.image.create(record)
-    return 
-
-
-def register_commands(app):
-    @app.cli.command()
-    @click.option('--images', default=10, help='Quantity of images, default is 20.')
-    def forge(images):
-        click.echo('Generating the images...')
-        fake_images()
-
-
 from flask_session import Session
 
 
@@ -46,8 +22,6 @@ def create_database(application):
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 0
     db.init_app(app)
     Session(app)  #  session load the config values from flask application config
     module = import_module('app.api.routes')
